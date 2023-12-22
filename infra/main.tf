e1cec7f2c596a1ee7db3b8d04b0b821d0bb912b2
Nazar Ivato <nazarivato@gmail.com>
2023-12-22 17:27:34 +0200

    Enabling loggind for control plane

--- infra/main.tf
+++ infra/main.tf
@@ -13,23 +13,27 @@ provider "aws" {
   profile = "terraform"
 }
 
+locals {
+  prefix = "devops-finals"
+}
+
 module "vpc" {
   source = "./modules/vpc"
   providers = {
     aws = aws.frankfurt
   }
-  name_prefix = "devops-finals"
+  name_prefix = local.prefix
   vpc_cidr_block = "10.10.0.0/16"
   public_subnet_cidr_block = "10.10.1.0/24"
   first_private_subnet_cidr_block = "10.10.2.0/24"
   second_private_subnet_cidr_block = "10.10.3.0/24"
 }
 
-module "eks_cluster" {
+module "cluster" {
   source = "./modules/eks-cluster"
   providers = {
     aws = aws.frankfurt
   }
-  name_prefix = "devops-finals"
+  name_prefix = local.prefix
   subnet_ids = [module.vpc.first_private_subnet_id, module.vpc.second_private_subnet_id]
 }
--- infra/modules/eks-cluster/main.tf
+++ infra/modules/eks-cluster/main.tf
@@ -9,6 +9,10 @@ terraform {
 
 data "aws_region" "current" {}
 
+locals {
+  cluster_name = "${var.name_prefix}-cluster"
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -30,16 +34,25 @@ resource "aws_iam_role_policy_attachment" "cluster_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
+resource "aws_cloudwatch_log_group" "eks_log_group" {
+  # The log group name format is /aws/eks/<cluster-name>/cluster
+  # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
+  name = "/aws/eks/${local.cluster_name}/cluster"
+  retention_in_days = 7
+  # ... potentially other configuration ...
+}
+
 resource "aws_eks_cluster" "created_cluster" {
-  name = "${var.name_prefix}-cluster"
+  name = local.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
   vpc_config {
     subnet_ids = var.subnet_ids
   }
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
     aws_iam_role_policy_attachment.cluster_policy_attachment,
-#    aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController, TODO: figure out what is this
+    aws_cloudwatch_log_group.eks_log_group
   ]
 }
