0087b0d01ef8b0c0ef16330162ea832aa82dd9c4
Nazar Ivato <nazarivato@gmail.com>
2023-12-22 22:37:10 +0200

    Fix coredns version

--- infra/modules/eks-cluster/main.tf
+++ infra/modules/eks-cluster/main.tf
@@ -71,7 +71,7 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name = aws_eks_cluster.created_cluster.name
   addon_name = "coredns"
-  addon_version = "v1.10.1-eksbuild.1"
+  addon_version = "v1.10.1-eksbuild.2"
 }
 
 resource "aws_eks_addon" "kube_proxy" {
